e74e1624bc66e745e6c4333780624b7eee3a1beb
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-08-01 20:36:45 +0400

    Draft

---
+++ README.md
@@ -0,0 +1,4 @@
+# CTF.Moscow 2019 101
+
+## License
+MIT @ [VolgaCTF](https://github.com/aspyatkin)
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'ctf-moscow-2019-101'
+description 'Installs and configures 101.ctf.moscow'
+version '1.0.0'
+
+recipe 'ctf-moscow', 'Installs and configures 101.ctf.moscow'
+depends 'ngx', '>= 2.1.0'
+depends 'tls', '>= 3.2.0'
